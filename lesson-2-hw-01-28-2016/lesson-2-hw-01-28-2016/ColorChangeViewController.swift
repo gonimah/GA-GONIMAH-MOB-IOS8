a3da38c89ad8883414ce382266c82f5509f22e4c
Mayada Gonimah <mayada.gonimah@nytimes.com>
2016-01-30 23:49:31 -0500

    Update colors and ui

--- lesson-2-hw-01-28-2016/lesson-2-hw-01-28-2016/ColorChangeViewController.swift
+++ lesson-2-hw-01-28-2016/lesson-2-hw-01-28-2016/ColorChangeViewController.swift
@@ -1,40 +1,24 @@
-//
-//  ColorChangeViewController.swift
-//  lesson-2-hw-01-28-2016
-//
-//  Created by Gonimah, Mayada on 1/30/16.
-//  Copyright © 2016 Gonimah, Mayada. All rights reserved.
-//
-
 import UIKit
 
 class ColorChangeViewController: UIViewController {
-
     @IBOutlet var colorOutputView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
     @IBAction func changeColor(sender: AnyObject) {
-        colorOutputView.backgroundColor = UIColor.brownColor()
+        colorOutputView.backgroundColor = randomColor()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func randomColor() -> UIColor {
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
-    */
-
 }
