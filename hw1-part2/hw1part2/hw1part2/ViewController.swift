1d9d92be2a92860a1789c4d9c0e357bbd53af7cf
Mayada Gonimah <mayada.gonimah@nytimes.com>
2016-02-09 16:07:42 -0500

    homework 1 part 2 initial comit

---
+++ hw1-part2/hw1part2/hw1part2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1part2
+//
+//  Created by Gonimah, Mayada on 2/9/16.
+//  Copyright © 2016 Gonimah, Mayada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
